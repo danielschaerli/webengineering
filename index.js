50e7183fc809168d8921b3417208ddaf84cba63d
danielschaerli <daniel.schaerli@hispeed.ch>
2021-08-25 17:47:30 +0200

    correted path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.send(__dirname + 'index.html');
+    res.send(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
