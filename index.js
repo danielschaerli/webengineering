d994a10ee918ba251d99133b860f9bdaadbbb4f0
danielschaerli <daniel.schaerli@hispeed.ch>
2021-08-28 15:39:30 +0200

    correted path

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
+    res.send('<h1>Hello world</h1>');
 });
 
 server.listen(3000, () => {
